4b8255259506e4a3ff0867c1761b86a615470d19
Kasper Ziemianek <kasper.ziemianek@gmail.com>
2020-02-18 20:43:02 +0100

    remove unused index field

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -5,14 +5,13 @@ pub struct TspData {
 
 #[derive(Debug, PartialEq)]
 pub struct NodeCoord {
-    pub index: i32,
     pub x: f32,
     pub y: f32,
 }
 
 impl NodeCoord {
-    fn new(index: i32, x: f32, y: f32) -> NodeCoord {
-        NodeCoord { index, x, y }
+    fn new(x: f32, y: f32) -> NodeCoord {
+        NodeCoord { x, y }
     }
 }
 
@@ -58,13 +57,12 @@ fn parse_node_coord_section_line(line: &str) -> Result<NodeCoord, &'static str> 
         .split_whitespace()
         .map(|s| s.parse::<f32>().expect("Node coord must be numerical"))
         .collect();
-    if parsed_line.len() < 3 {
+    if parsed_line.len() < 2 {
         return Err("line too short");
     }
     Ok(NodeCoord::new(
-        parsed_line.get(0).unwrap().to_owned() as i32,
+        parsed_line.get(0).unwrap().to_owned(),
         parsed_line.get(1).unwrap().to_owned(),
-        parsed_line.get(2).unwrap().to_owned(),
     ))
 }
 
@@ -72,48 +70,47 @@ fn parse_node_coord_section_line(line: &str) -> Result<NodeCoord, &'static str> 
 mod tests {
     use super::*;
     #[test]
-    #[test]
     fn dj38() {
         let data = read(&"dj38.tsp").unwrap();
         let expected_node_coords = vec![
-            NodeCoord::new(1, 11003.611100, 42102.500000),
-            NodeCoord::new(2, 11108.611100, 42373.888900),
-            NodeCoord::new(3, 11133.333300, 42885.833300),
-            NodeCoord::new(4, 11155.833300, 42712.500000),
-            NodeCoord::new(5, 11183.333300, 42933.333300),
-            NodeCoord::new(6, 11297.500000, 42853.333300),
-            NodeCoord::new(7, 11310.277800, 42929.444400),
-            NodeCoord::new(8, 11416.666700, 42983.333300),
-            NodeCoord::new(9, 11423.888900, 43000.277800),
-            NodeCoord::new(10, 11438.333300, 42057.222200),
-            NodeCoord::new(11, 11461.111100, 43252.777800),
-            NodeCoord::new(12, 11485.555600, 43187.222200),
-            NodeCoord::new(13, 11503.055600, 42855.277800),
-            NodeCoord::new(14, 11511.388900, 42106.388900),
-            NodeCoord::new(15, 11522.222200, 42841.944400),
-            NodeCoord::new(16, 11569.444400, 43136.666700),
-            NodeCoord::new(17, 11583.333300, 43150.000000),
-            NodeCoord::new(18, 11595.000000, 43148.055600),
-            NodeCoord::new(19, 11600.000000, 43150.000000),
-            NodeCoord::new(20, 11690.555600, 42686.666700),
-            NodeCoord::new(21, 11715.833300, 41836.111100),
-            NodeCoord::new(22, 11751.111100, 42814.444400),
-            NodeCoord::new(23, 11770.277800, 42651.944400),
-            NodeCoord::new(24, 11785.277800, 42884.444400),
-            NodeCoord::new(25, 11822.777800, 42673.611100),
-            NodeCoord::new(26, 11846.944400, 42660.555600),
-            NodeCoord::new(27, 11963.055600, 43290.555600),
-            NodeCoord::new(28, 11973.055600, 43026.111100),
-            NodeCoord::new(29, 12058.333300, 42195.555600),
-            NodeCoord::new(30, 12149.444400, 42477.500000),
-            NodeCoord::new(31, 12286.944400, 43355.555600),
-            NodeCoord::new(32, 12300.000000, 42433.333300),
-            NodeCoord::new(33, 12355.833300, 43156.388900),
-            NodeCoord::new(34, 12363.333300, 43189.166700),
-            NodeCoord::new(35, 12372.777800, 42711.388900),
-            NodeCoord::new(36, 12386.666700, 43334.722200),
-            NodeCoord::new(37, 12421.666700, 42895.555600),
-            NodeCoord::new(38, 12645.000000, 42973.333300),
+            NodeCoord::new(11003.611100, 42102.500000),
+            NodeCoord::new(11108.611100, 42373.888900),
+            NodeCoord::new(11133.333300, 42885.833300),
+            NodeCoord::new(11155.833300, 42712.500000),
+            NodeCoord::new(11183.333300, 42933.333300),
+            NodeCoord::new(11297.500000, 42853.333300),
+            NodeCoord::new(11310.277800, 42929.444400),
+            NodeCoord::new(11416.666700, 42983.333300),
+            NodeCoord::new(11423.888900, 43000.277800),
+            NodeCoord::new(11438.333300, 42057.222200),
+            NodeCoord::new(11461.111100, 43252.777800),
+            NodeCoord::new(11485.555600, 43187.222200),
+            NodeCoord::new(11503.055600, 42855.277800),
+            NodeCoord::new(11511.388900, 42106.388900),
+            NodeCoord::new(11522.222200, 42841.944400),
+            NodeCoord::new(11569.444400, 43136.666700),
+            NodeCoord::new(11583.333300, 43150.000000),
+            NodeCoord::new(11595.000000, 43148.055600),
+            NodeCoord::new(11600.000000, 43150.000000),
+            NodeCoord::new(11690.555600, 42686.666700),
+            NodeCoord::new(11715.833300, 41836.111100),
+            NodeCoord::new(11751.111100, 42814.444400),
+            NodeCoord::new(11770.277800, 42651.944400),
+            NodeCoord::new(11785.277800, 42884.444400),
+            NodeCoord::new(11822.777800, 42673.611100),
+            NodeCoord::new(11846.944400, 42660.555600),
+            NodeCoord::new(11963.055600, 43290.555600),
+            NodeCoord::new(11973.055600, 43026.111100),
+            NodeCoord::new(12058.333300, 42195.555600),
+            NodeCoord::new(12149.444400, 42477.500000),
+            NodeCoord::new(12286.944400, 43355.555600),
+            NodeCoord::new(12300.000000, 42433.333300),
+            NodeCoord::new(12355.833300, 43156.388900),
+            NodeCoord::new(12363.333300, 43189.166700),
+            NodeCoord::new(12372.777800, 42711.388900),
+            NodeCoord::new(12386.666700, 43334.722200),
+            NodeCoord::new(12421.666700, 42895.555600),
+            NodeCoord::new(12645.000000, 42973.333300),
         ];
         assert_eq!(data.node_coords, expected_node_coords);
     }
